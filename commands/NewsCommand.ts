84bab0016a834b672a7558518fb01f69b4d24b1c
Kapil <gautime.idk@gmail.com>
2019-05-04 18:39:27 +0530

    Improved News Integration

---
+++ commands/NewsCommand.ts
@@ -0,0 +1,52 @@
+import { IHttp, IModify, IPersistence, IRead } from '@rocket.chat/apps-engine/definition/accessors';
+import { ISlashCommand, ISlashCommandPreview, ISlashCommandPreviewItem, SlashCommandContext } from '@rocket.chat/apps-engine/definition/slashcommands';
+import { NewsApp } from '../News';
+import { NewsResult } from '../helpers/NewsResult';
+import { NewsGetter } from '../helpers/NewsGetter';
+import { formatWithOptions } from 'util';
+
+
+export class NewsCommand implements ISlashCommand {
+  public command = 'news';
+  public i18nParamsExample = 'news_search_term';
+  public i18nDescription = 'news_description';
+  public providesPreview = false;
+
+  constructor(private readonly app: NewsApp) { }
+
+  public async executor(context: SlashCommandContext, read: IRead, modify: IModify, http: IHttp, persis: IPersistence): Promise<void> {
+
+    const parame = context.getArguments().join(' ');
+
+    if (!parame) {
+      throw new Error('Method not implemented.');
+    }
+
+    const mg = modify.getCreator().startMessage().setSender(context.getSender()).setRoom(context.getRoom());
+    try {
+      const term = context.getArguments().join(' ').trim();
+
+       const wik = await this.app.getNewsGetter().getOne(this.app.getLogger(), http, term, read);
+
+      
+      mg.addAttachment({
+        title: {
+            value: ( wik.title.trim()),
+        },
+        thumbnailUrl: wik.url,
+        
+    });
+
+
+      await modify.getCreator().finish(mg);
+    } catch (e) {
+      this.app.getLogger().error('Failed getting text', e);
+      mg.setText('An error occurred when trying to send text :');
+
+      modify.getNotifier().notifyUser(context.getSender(), mg.getMessage());
+    }
+  }
+
+}
+
+
---
+++ helpers/NewsGetter.ts
@@ -0,0 +1,38 @@
+import { HttpStatusCode, IHttp, ILogger, IRead } from '@rocket.chat/apps-engine/definition/accessors';
+import { NewsResult } from '../helpers/NewsResult';
+
+
+export class NewsGetter {
+    private readonly url = 'https://content.guardianapis.com/search?';
+    private readonly defaultKey = 'da284288-9b7f-4abc-853d-dea33e0eec2d';
+    
+
+
+    public async getOne(logger: ILogger, http: IHttp, phrase: any, read: IRead): Promise<NewsResult> {
+
+        let search = phrase.trim();
+        if (!search) {
+            search = 'random';
+        }
+        const key = await read.getEnvironmentReader().getSettings().getValueById('news_apikey') || this.defaultKey;
+
+        const response = await http.get(`${this.url}q=${search}&format=json&api-key=${key}`);
+        try {
+            if (response.statusCode !== HttpStatusCode.OK || !response.data) {
+                logger.debug('Did not get a valid response', response);
+                throw new Error('Unable to retrieve the text.');
+            } else if (typeof response.data.response !== 'object') {
+                logger.debug('The response data is not an Object:', response.data);
+                throw new Error('Data is in a format we don\'t understand.');
+            }
+            
+            return response.data.response.map((r) => new NewsResult(r));
+
+        } catch (e) {
+            return new NewsResult(response.data);
+        }
+        //console.log('The returned data:', response.data.query);
+    }
+}
+
+
